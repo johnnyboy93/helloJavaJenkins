68a918fabafd63dd70f956a1d045780a3cbac3e2
johnnyboy93 <johnconstantinedewitt@gmail.com>
2020-07-29 10:55:42 +0800

    branch 2 commit

--- src/main/java/javaJenkins/HelloJava.java
+++ src/main/java/javaJenkins/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("HELLO JAVA JENKINS!");
 		System.out.println("Create new");
+		System.out.println("branch 2");
 
 	}
 
