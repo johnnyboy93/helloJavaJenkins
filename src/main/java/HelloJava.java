f2c9a6d2b688287ef94cf91bb1469ad0efaa5331
johnnyboy93 <johnconstantinedewitt@gmail.com>
2020-07-30 10:47:34 +0800

    omg

---
+++ src/main/java/HelloJava.java
@@ -0,0 +1,15 @@
+
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO JAVA JENKINS!");
+		System.out.println("Create new");
+		System.out.println("branch 2");
+
+	}
+	public static String msg() {
+		return "Hello";
+	}
+
+}
--- src/test/java/DemoApp.java
+++ src/test/java/DemoApp.java
@@ -1,4 +1,4 @@
-package javaJenkins;
+
 
 import static org.junit.Assert.*;
 
